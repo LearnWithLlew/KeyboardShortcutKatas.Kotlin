a4ec5dd5eab4bbe41fd8e336a90c6d29722579d7
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-09-27 11:30:59 -0600

    initiial commit

---
+++ src/test/kotlin/org/learnwithllew/keyboardshortcutskata/InlineTest.kt
@@ -0,0 +1,11 @@
+package org.learnwithllew.keyboardshortcutskata
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class InlineTest {
+    @Test
+    internal fun testInline() {
+    assertEquals(42, 43)
+    }
+}
